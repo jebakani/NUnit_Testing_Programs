310e11957b5f8bc5eddcdb1c726f780f53babe5f
Jebakani <vjishwarya@gmail.com>
2021-07-01 14:57:11 +0530

    [JEBAKANI] P2-Finding the day of week for given date

--- NUnitTestingPrograms/Program.cs
+++ NUnitTestingPrograms/Program.cs
@@ -4,6 +4,7 @@ namespace NUnitTestingPrograms
 {
     /// <summary>
     /// P1- Genrating changes for the given amount in vending machine
+    /// P2- Computing the date to display the day using Gregorian calendar formula
     /// </summary>
     class Program
     {
@@ -11,6 +12,7 @@ namespace NUnitTestingPrograms
         {
             Console.WriteLine("Welcome to N Unit Testing Programs");
             Console.WriteLine("1 - Vending Machine");
+            Console.WriteLine("2 - Day of week");
             Console.WriteLine("Enter you choice:");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
@@ -18,6 +20,11 @@ namespace NUnitTestingPrograms
                 case 1:
                     VendingMachine vendingMachine = new VendingMachine();
                     vendingMachine.ReadInput();
+                    break;
+
+                case 2:
+                    DayOfWeek dayOfWeek = new DayOfWeek();
+                    dayOfWeek.ReadInput();
                     break;
 
                 default:
