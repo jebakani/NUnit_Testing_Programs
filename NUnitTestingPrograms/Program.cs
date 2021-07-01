0a67ae2314c67cbfc1b4e6eac2651ee4c2dba451
Jebakani <vjishwarya@gmail.com>
2021-07-01 16:25:14 +0530

    [JEBAKANI] P6-Converting Decimal number into Binary

--- NUnitTestingPrograms/Program.cs
+++ NUnitTestingPrograms/Program.cs
@@ -8,6 +8,7 @@ namespace NUnitTestingPrograms
     /// P3- Converts the temperature as celsius to fahrenheit and vice verse
     /// P4- Calculating the monthly payment 
     /// P5- Calculating square root of the number
+    /// P6- Decimal to binary conversion
     /// </summary>
     class Program
     {
@@ -19,6 +20,7 @@ namespace NUnitTestingPrograms
             Console.WriteLine("3 - Temperature Conversion");
             Console.WriteLine("4 - Monthly Payment");
             Console.WriteLine("5 - Square Root");
+            Console.WriteLine("6 - Decimal to binary Conversion");
             Console.WriteLine("Enter you choice:");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
@@ -44,6 +46,11 @@ namespace NUnitTestingPrograms
 
                 case 5:
                     SquareRoot.ReadInput();
+                    break;
+
+                case 6:
+                    DecimalToBinary decimalToBinary = new DecimalToBinary();
+                    decimalToBinary.ReadInput();
                     break;
 
                 default:
