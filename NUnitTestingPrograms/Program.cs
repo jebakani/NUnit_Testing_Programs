34f76a11e0d9522ee8cf2034f4a6b3bb9b65b0f4
Jebakani <vjishwarya@gmail.com>
2021-07-01 15:17:26 +0530

    [JEBAKANI] P3- Executing Temperature Convertion

--- NUnitTestingPrograms/Program.cs
+++ NUnitTestingPrograms/Program.cs
@@ -5,6 +5,7 @@ namespace NUnitTestingPrograms
     /// <summary>
     /// P1- Genrating changes for the given amount in vending machine
     /// P2- Computing the date to display the day using Gregorian calendar formula
+    /// P3- Converts the temperature as celsius to fahrenheit and vice verse
     /// </summary>
     class Program
     {
@@ -13,6 +14,7 @@ namespace NUnitTestingPrograms
             Console.WriteLine("Welcome to N Unit Testing Programs");
             Console.WriteLine("1 - Vending Machine");
             Console.WriteLine("2 - Day of week");
+            Console.WriteLine("3 - Temperature Conversion");
             Console.WriteLine("Enter you choice:");
             int choice = Convert.ToInt32(Console.ReadLine());
             switch(choice)
@@ -25,6 +27,11 @@ namespace NUnitTestingPrograms
                 case 2:
                     DayOfWeek dayOfWeek = new DayOfWeek();
                     dayOfWeek.ReadInput();
+                    break;
+
+                case 3:
+                    TemperaturConversion temperaturConversion = new TemperaturConversion();
+                    temperaturConversion.ReadInput();
                     break;
 
                 default:
