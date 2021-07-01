5f7bda304319d0811af85163876d96a81d92bcb5
Jebakani <vjishwarya@gmail.com>
2021-07-01 14:27:34 +0530

    [JEBAKANI] P1-Generating number of notes returned

--- NUnitTestingPrograms/Program.cs
+++ NUnitTestingPrograms/Program.cs
@@ -2,11 +2,27 @@
 
 namespace NUnitTestingPrograms
 {
+    /// <summary>
+    /// P1- Genrating changes for the given amount in vending machine
+    /// </summary>
     class Program
     {
         static void Main(string[] args)
         {
             Console.WriteLine("Welcome to N Unit Testing Programs");
+            Console.WriteLine("1 - Vending Machine");
+            Console.WriteLine("Enter you choice:");
+            int choice = Convert.ToInt32(Console.ReadLine());
+            switch(choice)
+            {
+                case 1:
+                    VendingMachine vendingMachine = new VendingMachine();
+                    vendingMachine.ReadInput();
+                    break;
+
+                default:
+                    break;
+            }
             Console.Read();
         }
     }
