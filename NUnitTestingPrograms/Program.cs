19e450fd3d6cb0975c2bd9b53e668364b0efd9b9
Jebakani <vjishwarya@gmail.com>
2021-07-01 13:04:50 +0530

    [JEBAKANI] Adding the NUnit testing Program

---
+++ NUnitTestingPrograms/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace NUnitTestingPrograms
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to N Unit Testing Programs");
+            Console.Read();
+        }
+    }
+}
